647056011f793296e9309e51660f984482dfa81b
Paolo Savoldi <paooolino@gmail.com>
2017-06-06 16:37:59 +0200

    routes and templates

--- web/index.php
+++ web/index.php
@@ -1,14 +1,40 @@
 <?php
-require 'config.php';
-
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
+$container['view'] = new \Slim\Views\PhpRenderer("./templates/");
 
-    return $response;
+$app->get('/', function (Request $request, Response $response) {
+	$widgets = ["login", "register", "standings", "events"];
+	$response = $this->view->render($response, "layout.phtml", ["widgets" => $widgets]);
+	return $response;
 });
+
+$app->get('/leagues', function (Request $request, Response $response) {
+	$widgets = ["abstract"];
+	$response = $this->view->render($response, "layout.phtml", ["widgets" => $widgets]);
+	return $response;
+});
+
+$app->get('/league/{league_id}', function (Request $request, Response $response) {
+	$widgets = ["leagueinfo", "standings", "calendar"];
+	$response = $this->view->render($response, "layout.phtml", ["widgets" => $widgets]);
+	return $response;
+});
+
+$app->get('/team/{team_id}', function (Request $request, Response $response) {
+	$widgets = ["teamfile"];
+	$response = $this->view->render($response, "layout.phtml", ["widgets" => $widgets]);
+	return $response;
+});
+
+$app->get('/player/{player_id}', function (Request $request, Response $response) {
+	$widgets = ["playerfile"];
+	$response = $this->view->render($response, "layout.phtml", ["widgets" => $widgets]);
+	return $response;
+});
+
 $app->run();
