858819af0d8bdfa8266a35df1ae87a25e2b46f23
Paolo Savoldi <paooolino@gmail.com>
2017-06-09 15:01:19 +0200

    fixing composer phpunit configuration

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,22 +1,16 @@
 <?php
-use PHPUnit\Framework\TestCase;
-use PHPUnit\DbUnit\TestCaseTrait;
-
-class AppTest extends TestCase {
-	
+class AppTest extends PHPUnit_Extensions_Database_TestCase {
 	/**
 	 * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
 	 */
-	public function getConnection() {
-		$pdo = new PDO('sqlite::memory:');
-		return $this->createDefaultDBConnection($pdo, ':memory:');
+	public function getConnection()	{
+
 	}
-	
+
 	/**
 	 * @return PHPUnit_Extensions_Database_DataSet_IDataSet
 	 */
 	public function getDataSet() {
-		return $this->createFlatXMLDataSet(dirname(__FILE__).'/_files/guestbook-seed.xml');
+		
 	}
-	
 }
